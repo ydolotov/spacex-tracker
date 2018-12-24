f60445125ae0a6f03b1558f2b6ecd750e1ed853d
Yura Dolotov <ura.dolotov@mail.ru>
2018-12-24 22:40:08 +0300

    Add files via upload

---
+++ public/precache-manifest.3c81726aa584b670ad1c0f50afc640f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d09863d45dc1a8b0953b",
+    "url": "/static/css/main.bc0c895f.chunk.css"
+  },
+  {
+    "revision": "d09863d45dc1a8b0953b",
+    "url": "/static/js/main.d09863d4.chunk.js"
+  },
+  {
+    "revision": "5be207921493f3117b0d",
+    "url": "/static/js/1.5be20792.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "846f00da4955ea23a569f83f53509eeb",
+    "url": "/index.html"
+  }
+];
